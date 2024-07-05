898b1c7b4dcc09d1750c6524e1285c0fd55bb0e2
NjoguRufus <rsgxliv@gmail.com>
2024-07-05 10:09:06 +0300

    done

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
+
 metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
